9e80d52509c9031ff7d9afe76c51b531a46749d9
Javier-Fermin <javierferminardila64@gmail.com>
2023-09-22 13:56:26 +0200

    JavaFX Done

--- src/application/Application.java
+++ src/application/Application.java
@@ -3,19 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package helloworld;
+package application;
 
 import controller.Controller;
 import model.ModelFactory;
 import view.ViewFactory;
-import view.View;
-import model.Model;
 
 /**
  *
  * @author javie
  */
-public class HelloWorld {
+public class Application {
 
     /**
      * @param args the command line arguments
@@ -23,5 +21,4 @@ public class HelloWorld {
     public static void main(String[] args) {
         new Controller().run(ViewFactory.getView(), ModelFactory.getModel());
     }
-    
 }
--- src/view/WindowViewImplementation.java
+++ src/view/WindowViewImplementation.java
@@ -5,15 +5,61 @@
  */
 package view;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  *
  * @author javie
  */
-public class WindowViewImplementation implements View{
+public class WindowViewImplementation extends javafx.application.Application implements View,Initializable{
+
+    @FXML
+    private Label label;
+    private String greeting;
+    
+    @Override
+    public void showGreeting(String greeting) {
+        //Stage stage = new Stage();
+        try {
+            String[] args = new String[1];
+            args[0] = greeting;
+            launch(args);
+            
+        } catch (Exception ex) {
+            Logger.getLogger(WindowViewImplementation.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     @Override
-    public void showGreeting(String view) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("View.fxml"));
+        Parent root = loader.load();
+        WindowViewImplementation controller = loader.getController();
+        controller.setGreeting(getParameters().getRaw().get(0));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.showAndWait();
+    }
+
+    
+    public void setGreeting(String greeting){
+        label.setText(greeting);
+    }
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO
     }
     
 }
